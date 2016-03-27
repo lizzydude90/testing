4f34fa3130b5fdcd3cd5b8b021ed2d033c87c41c
Liz E <lizzydude90@users.noreply.github.com>
2016-03-27 22:34:54 +0800

    Update firstswitch.c

--- firstswitch.c
+++ firstswitch.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
+#include <cs50.h>
 
+int main(void)
+{
+  printf("What day is it tomorrow? Type a day number. ")
+  int d = GetInt();
+  
+  switch (d)
+  {
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+      printf("Stop being a nerd and prepare for work!\n");
+      break;
+      
+    case 6:
+    case 7:
+      printf("Yay! It's the weekend!\n");
+      break;
+      
+    default:
+      printf("That is not a valid day number.\n");
+      break;
+  }
+}
